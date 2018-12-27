b54c6ced0f0f6555df2359edc16135ceb1a2d879
Will Breaden Madden <wbm@protonmail.ch>
2018-12-27 17:28:50 +0000

    add README.md, add setup.sh

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo "install Chromium extensions"
+chromium-browser --new-window                                                                           \
+    https://chrome.google.com/webstore/detail/awesome-screenshot-screen/nlipoenfbbikpbjkfpfillcgkoblgpmj\
+    https://chrome.google.com/webstore/detail/bandcamp-downloader/nmoobgpmablfmgchfjnhkbloaobiogeh      \
+    https://chrome.google.com/webstore/detail/dark-reader/eimadpbcbfnmbkopoojfekhnkhdbieeh              \
+    https://chrome.google.com/webstore/detail/empty-new-tab-page/dpjamkmjmigaoobjbekmfgabipmfilij       \
+    https://chrome.google.com/webstore/detail/enable-right-click/hhojmcideegachlhfgfdhailpfhgknjm       \
+    https://chrome.google.com/webstore/detail/full-page-screen-capture/fdpohaocaechififmbbbbbknoalclacl \
+    https://chrome.google.com/webstore/detail/history-onoff/ljbpakpmiimdmblcjjhhbfabbkmcgmdp            \
+    https://chrome.google.com/webstore/detail/image-downloader/cnpniohnfphhjihaiiggeabnkjhpaldj         \
+    https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn                 \
+    https://chrome.google.com/webstore/detail/perceptual-ad-highlighter/mahgiflleahghaapkboihnbhdplhnchp\
+    https://chrome.google.com/webstore/detail/reddit-enhancement-suite/kbmfpngjjgdllneeigpgjifpgocmfgmb \
+    https://chrome.google.com/webstore/detail/reload-all-tabs/midkcinmplflbiflboepnahkboeonkam          \
+    https://chrome.google.com/webstore/detail/session-buddy/edacconmaakjimmfgnblocblbcdcpbko            \
+    https://chrome.google.com/webstore/detail/ublock-origi/cjpalhdlnbpafiamejdnhcphjbkeiagm             \
+    https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf                  \
+    https://chrome.google.com/webstore/detail/video-gif-downloader-for/ajanondpapegkikdhmmhmoogcaajdokn
+echo "install Firefox extensions"
+firefox                                                                                                 \
+    https://addons.mozilla.org/en-US/firefox/addon/absolute-enable-right-click                          \
+    https://addons.mozilla.org/en-US/firefox/addon/bypasspaywalls                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/cookie-autodelete                                    \
+    https://addons.mozilla.org/en-US/firefox/addon/darkreader                                           \
+    https://addons.mozilla.org/en-US/firefox/addon/decentraleyes                                        \
+    https://addons.mozilla.org/en-US/firefox/addon/ether-metamask                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/fvd-speed-dial                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/image-picker                                         \
+    https://addons.mozilla.org/en-US/firefox/addon/nuke-anything-enhanced                               \
+    https://addons.mozilla.org/en-US/firefox/addon/privacy-possum                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/reddit-enhancement-suite                             \
+    https://addons.mozilla.org/en-US/firefox/addon/reload-all-tabs                                      \
+    https://addons.mozilla.org/en-US/firefox/addon/session-manager                                      \
+    https://addons.mozilla.org/en-US/firefox/addon/startpage-https-privacy-search                       \
+    https://addons.mozilla.org/en-US/firefox/addon/text-multicopy                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/tumblr-high-quality                                  \
+    https://addons.mozilla.org/en-US/firefox/addon/ublock-origin                                        \
+    https://addons.mozilla.org/en-US/firefox/addon/umatrix                                              \
+    https://addons.mozilla.org/en-US/firefox/addon/video-downloadhelper
