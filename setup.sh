102dffff33afbeb1f1b95fbc61c480632962de6f
Will Breaden Madden <wbm@protonmail.ch>
2019-01-23 19:50:16 +0000

    add NewsGuard, prioritise Firefox over Chromium

--- setup.sh
+++ setup.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+echo "install Firefox add-ons"
+firefox                                                                                                 \
+    https://addons.mozilla.org/en-GB/firefox/addon/old-reddit-redirect                                  \
+    https://addons.mozilla.org/en-US/firefox/addon/absolute-enable-right-click                          \
+    https://addons.mozilla.org/en-US/firefox/addon/bypasspaywalls                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/cookie-autodelete                                    \
+    https://addons.mozilla.org/en-US/firefox/addon/darkreader                                           \
+    https://addons.mozilla.org/en-US/firefox/addon/decentraleyes                                        \
+    https://addons.mozilla.org/en-US/firefox/addon/ether-metamask                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/fvd-speed-dial                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/image-picker                                         \
+    https://addons.mozilla.org/en-US/firefox/addon/newsguard                                            \
+    https://addons.mozilla.org/en-US/firefox/addon/nuke-anything-enhanced                               \
+    https://addons.mozilla.org/en-US/firefox/addon/privacy-possum                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/reddit-enhancement-suite                             \
+    https://addons.mozilla.org/en-US/firefox/addon/reload-all-tabs                                      \
+    https://addons.mozilla.org/en-US/firefox/addon/session-manager                                      \
+    https://addons.mozilla.org/en-US/firefox/addon/startpage-https-privacy-search                       \
+    https://addons.mozilla.org/en-US/firefox/addon/text-multicopy                                       \
+    https://addons.mozilla.org/en-US/firefox/addon/tumblr-high-quality                                  \
+    https://addons.mozilla.org/en-US/firefox/addon/ublock-origin                                        \
+    https://addons.mozilla.org/en-US/firefox/addon/umatrix                                              \
+    https://addons.mozilla.org/en-US/firefox/addon/video-downloadhelper
 echo "install Chromium extensions"
 chromium-browser --new-window                                                                           \
     https://chrome.google.com/webstore/detail/awesome-screenshot-screen/nlipoenfbbikpbjkfpfillcgkoblgpmj\
@@ -10,6 +33,7 @@ chromium-browser --new-window                                                   
     https://chrome.google.com/webstore/detail/history-onoff/ljbpakpmiimdmblcjjhhbfabbkmcgmdp            \
     https://chrome.google.com/webstore/detail/image-downloader/cnpniohnfphhjihaiiggeabnkjhpaldj         \
     https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn                 \
+    https://chrome.google.com/webstore/detail/newsguard/hcgajcpgaalgpeholhdooeddllhedegi                \
     https://chrome.google.com/webstore/detail/perceptual-ad-highlighter/mahgiflleahghaapkboihnbhdplhnchp\
     https://chrome.google.com/webstore/detail/reddit-enhancement-suite/kbmfpngjjgdllneeigpgjifpgocmfgmb \
     https://chrome.google.com/webstore/detail/reload-all-tabs/midkcinmplflbiflboepnahkboeonkam          \
@@ -17,25 +41,3 @@ chromium-browser --new-window                                                   
     https://chrome.google.com/webstore/detail/ublock-origi/cjpalhdlnbpafiamejdnhcphjbkeiagm             \
     https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf                  \
     https://chrome.google.com/webstore/detail/video-gif-downloader-for/ajanondpapegkikdhmmhmoogcaajdokn
-echo "install Firefox extensions"
-firefox                                                                                                 \
-    https://addons.mozilla.org/en-US/firefox/addon/absolute-enable-right-click                          \
-    https://addons.mozilla.org/en-US/firefox/addon/bypasspaywalls                                       \
-    https://addons.mozilla.org/en-US/firefox/addon/cookie-autodelete                                    \
-    https://addons.mozilla.org/en-US/firefox/addon/darkreader                                           \
-    https://addons.mozilla.org/en-US/firefox/addon/decentraleyes                                        \
-    https://addons.mozilla.org/en-US/firefox/addon/ether-metamask                                       \
-    https://addons.mozilla.org/en-US/firefox/addon/fvd-speed-dial                                       \
-    https://addons.mozilla.org/en-US/firefox/addon/image-picker                                         \
-    https://addons.mozilla.org/en-US/firefox/addon/nuke-anything-enhanced                               \
-    https://addons.mozilla.org/en-US/firefox/addon/privacy-possum                                       \
-    https://addons.mozilla.org/en-US/firefox/addon/reddit-enhancement-suite                             \
-    https://addons.mozilla.org/en-GB/firefox/addon/old-reddit-redirect                                  \
-    https://addons.mozilla.org/en-US/firefox/addon/reload-all-tabs                                      \
-    https://addons.mozilla.org/en-US/firefox/addon/session-manager                                      \
-    https://addons.mozilla.org/en-US/firefox/addon/startpage-https-privacy-search                       \
-    https://addons.mozilla.org/en-US/firefox/addon/text-multicopy                                       \
-    https://addons.mozilla.org/en-US/firefox/addon/tumblr-high-quality                                  \
-    https://addons.mozilla.org/en-US/firefox/addon/ublock-origin                                        \
-    https://addons.mozilla.org/en-US/firefox/addon/umatrix                                              \
-    https://addons.mozilla.org/en-US/firefox/addon/video-downloadhelper
